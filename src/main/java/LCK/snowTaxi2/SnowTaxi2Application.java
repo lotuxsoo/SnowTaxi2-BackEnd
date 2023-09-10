6c91e2b3bfca558bf5451bd0fdf00bb08841406b
kelly1422 <kelly1422@naver.com>
2023-09-11 04:34:21 +0900

    프로젝트 초기 설정

---
+++ src/main/java/LCK/snowTaxi2/SnowTaxi2Application.java
@@ -0,0 +1,13 @@
+package LCK.snowTaxi2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SnowTaxi2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SnowTaxi2Application.class, args);
+	}
+
+}
---
+++ src/test/java/LCK/snowTaxi2/SnowTaxi2ApplicationTests.java
@@ -0,0 +1,13 @@
+package LCK.snowTaxi2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SnowTaxi2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
