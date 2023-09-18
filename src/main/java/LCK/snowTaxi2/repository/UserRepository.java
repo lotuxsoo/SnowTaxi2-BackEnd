b4e736344433456d2d8224443d6c1a72f25fed94
kelly1422 <kelly1422@naver.com>
2023-09-19 02:45:16 +0900

    [ADD]: 의존성 추가

--- src/main/java/LCK/snowTaxi2/repository/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package LCK.snowTaxi2.repository;
-
-import LCK.snowTaxi2.domain.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-
-}
