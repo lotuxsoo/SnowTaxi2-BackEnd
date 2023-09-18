8c977e55fe4da21f47a3633ac8b65d53188b7cb5
kelly1422 <kelly1422@naver.com>
2023-09-18 23:05:13 +0900

    [ADD]: 기본 reposiotry 생성

---
+++ src/main/java/LCK/snowTaxi2/repository/ParticipationRepository.java
@@ -0,0 +1,7 @@
+package LCK.snowTaxi2.repository;
+
+import LCK.snowTaxi2.domain.Participation;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ParticipationRepository extends JpaRepository<Participation, Long> {
+}
---
+++ src/main/java/LCK/snowTaxi2/repository/TaxiPotRepository.java
@@ -0,0 +1,7 @@
+package LCK.snowTaxi2.repository;
+
+import LCK.snowTaxi2.domain.pot.TaxiPot;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TaxiPotRepository extends JpaRepository<TaxiPot, Long> {
+}
