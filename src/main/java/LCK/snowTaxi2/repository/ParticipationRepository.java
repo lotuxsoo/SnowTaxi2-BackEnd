1595f5076be856008f168766461782afda54f301
kelly1422 <kelly1422@naver.com>
2023-09-20 02:01:51 +0900

    [FEAT]: participation 생성, 조회, 삭제 서비스

--- src/main/java/LCK/snowTaxi2/repository/ParticipationRepository.java
+++ src/main/java/LCK/snowTaxi2/repository/ParticipationRepository.java
@@ -1,7 +1,12 @@
 package LCK.snowTaxi2.repository;
 
+import LCK.snowTaxi2.domain.Member;
 import LCK.snowTaxi2.domain.Participation;
+import LCK.snowTaxi2.domain.pot.TaxiPot;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ParticipationRepository extends JpaRepository<Participation, Long> {
+
+    void deleteParticipationByTaxiPotAndMember(TaxiPot taxiPot, Member member);
+
 }
---
+++ src/main/java/LCK/snowTaxi2/service/participation/ParticipationServiceImpl.java
@@ -0,0 +1,71 @@
+package LCK.snowTaxi2.service.participation;
+
+import LCK.snowTaxi2.domain.Member;
+import LCK.snowTaxi2.domain.Participation;
+import LCK.snowTaxi2.domain.pot.TaxiPot;
+import LCK.snowTaxi2.exception.NotFoundEntityException;
+import LCK.snowTaxi2.repository.MemberRepository;
+import LCK.snowTaxi2.repository.ParticipationRepository;
+import LCK.snowTaxi2.repository.TaxiPotRepository;
+import LCK.snowTaxi2.service.member.MemberService;
+import LCK.snowTaxi2.service.pot.TaxiPotService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class ParticipationServiceImpl implements ParticipationService {
+
+    private final ParticipationRepository participationRepository;
+    private final MemberRepository memberRepository;
+    private final TaxiPotRepository taxiPotRepository;
+    private final TaxiPotService taxiPotService;
+    private final MemberService memberService;
+
+    @Override
+    public boolean create(Long memberId, Long taxiPotId) {
+        Member member = memberRepository.findById(memberId).orElseThrow( () ->
+                new NotFoundEntityException("member id:", memberId.toString())
+        );
+
+        if (member.getParticipatingPotId() == 0) {
+            TaxiPot taxiPot = taxiPotRepository.findById(taxiPotId).orElseThrow( () ->
+                    new NotFoundEntityException("taxiPot id:", taxiPotId.toString())
+            );
+
+            Participation participation = Participation.builder()
+                    .member(member)
+                    .taxiPot(taxiPot)
+                    .build();
+
+            participationRepository.save(participation);
+            taxiPotService.changeHeadCount(1, taxiPotId);
+            memberService.setParticipatingPotId(memberId, taxiPotId);
+
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    @Override
+    public void delete(Long memberId) {
+        Member member = memberRepository.findById(memberId).orElseThrow( () ->
+                new NotFoundEntityException("member id:", memberId.toString())
+        );
+
+        Long taxiPotId = member.getParticipatingPotId();
+
+        TaxiPot taxiPot = taxiPotRepository.findById(taxiPotId).orElseThrow( () ->
+                new NotFoundEntityException("taxiPot id:", taxiPotId.toString())
+        );
+
+        participationRepository.deleteParticipationByTaxiPotAndMember(taxiPot, member);
+        memberService.setParticipatingPotId(memberId, 0);
+        taxiPotService.changeHeadCount(-1, taxiPotId);
+
+        if (taxiPot.getParticipations().size() == 0) {
+            taxiPotService.delete(taxiPotId);
+        }
+    }
+}
