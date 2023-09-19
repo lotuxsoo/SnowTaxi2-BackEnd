b07994e03ef58b0a59fa65f07f7be29c7d0967ac
kelly1422 <kelly1422@naver.com>
2023-09-20 02:01:08 +0900

    [FEAT]: 택시팟 생성, 조회, 삭제 서비스

--- src/main/java/LCK/snowTaxi2/dto/pot/TaxiPotResponseDto.java
+++ src/main/java/LCK/snowTaxi2/dto/pot/TaxiPotResponseDto.java
@@ -1,14 +1,29 @@
 package LCK.snowTaxi2.dto.pot;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.ToString;
+
+import java.time.LocalTime;
 
 @Getter
-public class TaxiPotResponseDto {
+@Builder
+@ToString
+public class TaxiPotResponseDto implements Comparable<TaxiPotResponseDto> {
 
     private long id;
 
-    private boolean isParticipate;
+    @JsonFormat(pattern = "HH:mm")
+    private LocalTime ridingTime;
 
-    private boolean isOver;
+    private int headCount;
 
+    private boolean isParticipating;
+
+    // 탑승 시간을 기준으로 정렬해주기 위해 추가
+    @Override
+    public int compareTo(TaxiPotResponseDto taxiPotResponseDto) {
+        return this.ridingTime.compareTo(taxiPotResponseDto.getRidingTime());
+    }
 }
--- src/main/java/LCK/snowTaxi2/service/pot/TaxiPotService.java
+++ src/main/java/LCK/snowTaxi2/service/pot/TaxiPotService.java
@@ -1,24 +1,30 @@
 package LCK.snowTaxi2.service.pot;
 
 import LCK.snowTaxi2.domain.Member;
-import LCK.snowTaxi2.domain.pot.TaxiPot;
 import LCK.snowTaxi2.dto.pot.TaxiPotResponseDto;
+import org.springframework.stereotype.Service;
 
-import java.time.LocalDateTime;
+import java.time.LocalTime;
 import java.util.List;
 
+@Service
 public interface TaxiPotService {
 
-    // 팟 생성하기
-    TaxiPotResponseDto create(int departureIdx, LocalDateTime ridingTime, long hostId);
+    // 팟 생성하기 (컨트롤러에서 택시팟 생성하고 반환받은 id 값으로 participationService.create 실행)
+    long create(String departure, LocalTime ridingTime);
 
-    // 택시 루트별로 오늘 모집중인 팟 정보들 모두 조회하기
-    List<TaxiPotResponseDto> getTodayPots(int departureIdx, long userId);
+    // 택시 루트별로 오늘 모집중인 팟 중 시간이 유효한 팟을 탑승시간을 기준으로 정렬해 리턴
+    List<TaxiPotResponseDto> getTodayPots(int departureIdx, Long memberId);
 
     // 현재 참여중인 팟 정보 불러오기
     TaxiPotResponseDto findParticipatingPot(Member member);
 
+    // 참여 기록 조회
+
+    // 팟에 참여하고 있는 인원 변경 ( +add )
+    void changeHeadCount(int add, Long id);
+
     // 생성한 팟 삭제 (시간 안지난 팟만 삭제 가능하도록)
-    void delete();
+    void delete(Long id);
 
 }
