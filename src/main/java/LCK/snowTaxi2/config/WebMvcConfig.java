90a5f9c87660cda375f4a378c74e480522c70bd4
kelly1422 <kelly1422@naver.com>
2023-09-24 00:12:46 +0900

    [FIX]: CORS 정책 수정

--- src/main/java/LCK/snowTaxi2/config/WebMvcConfig.java
+++ src/main/java/LCK/snowTaxi2/config/WebMvcConfig.java
@@ -12,7 +12,7 @@ public class WebMvcConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000")
+                .allowedOrigins("*")
                 .allowedMethods("*")
                 .allowedHeaders("*"); // 허용할 요청 헤더 목록
     }
