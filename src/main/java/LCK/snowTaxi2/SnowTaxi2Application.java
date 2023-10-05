24712e3abdb25d728f880c73e8407b1245fed781
0923ule <0923ule@gmail.com>
2023-10-05 23:34:07 +0900

    [FIX]: time zone

--- src/main/java/LCK/snowTaxi2/SnowTaxi2Application.java
+++ src/main/java/LCK/snowTaxi2/SnowTaxi2Application.java
@@ -4,6 +4,7 @@ import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Locale;
 import java.util.TimeZone;
 
 @SpringBootApplication
@@ -11,6 +12,7 @@ public class SnowTaxi2Application {
 	@PostConstruct
 	public void started() {
 		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+		Locale.setDefault(Locale.KOREA);
 
 	}
 	public static void main(String[] args) {
